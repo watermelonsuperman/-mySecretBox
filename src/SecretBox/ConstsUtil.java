a7f0b4777af95218cb72f80e5c8d223097785ddb
15538287230@163.com <15538287230@163.com>
2020-12-31 17:15:33 +0800

    打开盒子的方法

---
+++ src/SecretBox/ConstsUtil.java
@@ -0,0 +1,11 @@
+package SecretBox;
+
+public class ConstsUtil {
+    /**
+     * 箱子打开的标识字符串
+     */
+    public static final String OPENED_BOX_FLAG = "00000";
+    /**
+     *
+     */
+}
---
+++ src/SecretBox/OpenBox.java
@@ -0,0 +1,4 @@
+package SecretBox;
+
+public class OpenBox {
+}
